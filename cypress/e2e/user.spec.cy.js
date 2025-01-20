654095bd1b713f6e4d2987c67204567e1f1989a0
alvesfarias74 <alvesfarias74@gmail.com>
2025-01-20 20:03:56 -0300

    Include loginFail

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,7 +10,7 @@ const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
-  it.only('User Info Update - Success', () => {   
+  it('User Info Update - Success', () => {   
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
     
@@ -26,10 +26,8 @@ describe('Orange HRM Tests', () => {
   })
 
   it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type('Test')
-    cy.get(selectorList.passwordField).type('Test')
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert)
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userFail.username, userData.userFail.password)
+    loginPage.checkAccessInvalid()
   })
 })
