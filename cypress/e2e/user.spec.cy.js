6a6592de71d0a21562a346f440e548b579f8338e
alvesfarias74 <alvesfarias74@gmail.com>
2025-01-24 09:39:04 -0300

    add ramdon values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -18,8 +21,8 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
 
-    myInfoPage.fillPersonalDetails('Francisco', 'Alves', 'Farias')
-    myInfoPage.fillEmployeeDetail('12345', '1234',  '987456', '2025-03-25', '1974-03-24')
+    myInfoPage.fillPersonalDetails(chance.first(), chance.string(), chance.last())
+    myInfoPage.fillEmployeeDetail(chance.natural({ min: 1, max: 999 }), chance.bb_pin(),  chance.natural({ min: 1, max: 999 }), '2025-12-24', '1974-03-24')
     myInfoPage.fillStatus()
     myInfoPage.saveForm()     
   })
